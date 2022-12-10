2412d6fc3fdb0b1d54043f1b5694e72c7587ddf8
Dharrenz09 <dharrydharrenzerah@gmail.com>
2022-12-10 04:57:26 -0800

    Done

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,2 +1,2 @@
--- A script tthat displays the number with id = 89 in the tables first_table of the database hbtn_0c_0 in your MySQL server.
-SELECT COUNT(id) first_table WHERE id = 89;
+-- A script that displays the number with id = 89 in the tables first_table of the database hbtn_0c_0 in your MySQL server.
+SELECT COUNT(id) first_table WHERE id=89;
