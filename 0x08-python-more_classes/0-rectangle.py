17ab1bbd079fee71e943125cb710c31263988e40
Dharren09 <dharrydharrenzerah@gmail.com>
2022-12-20 22:57:25 +0300

    Done

--- 0x08-python-more_classes/0-rectangle.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""
-Contains a definition for an empty Class Rectangle.
-"""
-
-class Rectangle:
-    """Empty class"""
-    pass
--- 0x08-python-more_classes/README.md
+++
@@ -1,1 +0,0 @@
-Real definition of a rectangle
