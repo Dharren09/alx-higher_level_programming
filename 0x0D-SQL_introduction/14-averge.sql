3bf578188459334c8bbedeefa5a702bde365adca
Dharren Pius Makoha <107412228+Dharren09@users.noreply.github.com>
2023-01-30 18:56:01 +0300

    Done

--- 0x0D-SQL_introduction/14-averge.sql
+++ 0x0D-SQL_introduction/14-averge.sql
@@ -1,2 +1,3 @@
--- A script that combines the score average of all records in the table of the database hbtn_0c_0 in my MySQL server.
-SELECT AVG(score) AS average FROM second_table;
+-- computes the score average of all records in the table second_table
+
+SELECT AVG(score) 'average' FROM second_table;
