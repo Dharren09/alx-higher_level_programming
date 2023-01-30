2db4ec651ce113526737c509b347b4159d65b644
Dharren Pius Makoha <107412228+Dharren09@users.noreply.github.com>
2023-01-30 18:12:53 +0300

    Done

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
-let myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-for (let i = 0, len = myArray.length; i < len; i++) {
-  console.log(myArray[i]);
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+let lang;
+
+for (lang of languages) {
+  console.log(lang);
 }
