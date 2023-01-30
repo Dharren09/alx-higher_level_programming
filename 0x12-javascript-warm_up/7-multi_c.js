c6dc0d2a0f82179e8098bf9316beb805be62fb04
Dharren Pius Makoha <107412228+Dharren09@users.noreply.github.com>
2023-01-30 18:13:46 +0300

    Done

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-let x = process.argv[2];
-if (!isNaN(parseInt(process.argv[2]))){
-    for (let i = 0, i < x; i++) {
-        console.log('C is fun');
-    } 
-} else {
-    console.log('Missing number of occurrences');
+
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+let lang;
+
+for (lang of languages) {
+  console.log(lang);
 }
