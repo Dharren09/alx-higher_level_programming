f03885bc814635cb4688bec548a00ca2906d7fb4
Dharren Pius Makoha <107412228+Dharren09@users.noreply.github.com>
2023-01-30 18:54:39 +0300

    Done

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,2 +1,3 @@
--- A script that lists all records with score >= 10 in the table second_table of the dtabase hbtn_0c_0 in my MySQL server.
-SELECT score, name FROM second_table, WHERE score >= 10 ORDER BY DESC;
+-- lists all records with a score >= 10 in the table second_table
+
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
