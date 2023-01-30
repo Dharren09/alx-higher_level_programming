e564b816fb2e5b5b3e87f492acde0f2d1007fc8b
Dharren Pius Makoha <107412228+Dharren09@users.noreply.github.com>
2023-01-30 18:02:08 +0300

    Done

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
-exports.callMeMoby = function (var1, callback) {
-    for (let i = 0; i < var1; i++) {
-        callback(var1)
-    }
+
+exports.callMeMoby = function callMeMoby (x, theFunction) {
+  for (let i = 0; i < x; i++) {
+    theFunction();
+  }
 };
