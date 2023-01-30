59e210c28f8f87c32eff4a4c27f51b0c42c4cac9
Dharren Pius Makoha <107412228+Dharren09@users.noreply.github.com>
2023-01-30 18:12:00 +0300

    Done

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
-if (isNaN(parseInt(process.argv[2]))) {
-    console.log('Not a number');
-  } else {
-    console.log('My number: ' + parseInt(process.argv[2]));
-  }
+
+const args = process.argv;
+
+if (parseInt(args[2])) {
+  console.log('My number: ' + parseInt(args[2]));
+} else {
+  console.log('Not a number');
+}
