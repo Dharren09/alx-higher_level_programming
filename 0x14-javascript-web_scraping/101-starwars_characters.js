a01ac1e1e89cebe7a2425a1908c15d2af566673a
Dharrenz09 <dharrydharrenzerah@gmail.com>
2023-02-11 15:03:34 +0300

    Done

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -3,21 +3,25 @@
 
 const request = require('request');
 
-request(
-	`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`);
-	(error, response, body) => {
-		if (error) throw error;
-		else {
-			send(JSON.parse(body).characters, 0);
-		}
+request('https://swapi-api.hbtn.io/api/films/${process.argv[2]}', (err, response, body) => {
+	if (err) {
+		console.log(err);
+		return;
 	}
+	else {
+		send(JSON.parse(body).characters, 0);
+	}
+});
 
-function send (person, idx) {
+function send(person, idx) {
 	if (idx >= person.length) {
 		return;
 	}
-	request(person[idx], (error, response, body) => {
-		if (error) throw error;
+	request(person[idx], (err, ressponse, body) => {
+		if (err) {
+			console.log(err);
+			return;
+		}
 		else {
 			console.log(JSON.parse(body).name);
 			return send(person, ++idx);
